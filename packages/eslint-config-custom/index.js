9fd71244a0dbd51d7f6d376f85fa7fb2ea1ccabe
Samy Rahmani <samy.osmium@gmail.com>
2023-01-09 16:31:19 -0500

    Initialized with turbo

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ["next", "turbo", "prettier"],
   rules: {
+    "object-curly-spacing": ["error", "always"],
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
   },
