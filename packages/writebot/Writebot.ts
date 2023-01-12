9f02bae4921ccd4b51903826fff8e7753ad306f7
Samy Rahmani <samy.osmium@gmail.com>
2023-01-12 05:45:11 -0500

    Added abiltiy to use an Array as prompt to send multiple request

--- packages/writebot/Writebot.ts
+++ packages/writebot/Writebot.ts
@@ -2,39 +2,49 @@ import { assert, Infer, Struct, StructError } from 'superstruct';
 import { OpenAI } from './openai';
 
 export class Writebot {
-  private static types: WriterType[] | null;
+  private static presets: Preset[] | null;
   private static openai: OpenAI;
 
-  static initialize({ apiKey, types }: { apiKey: string, types?: any[] }) {
+  static initialize({ apiKey, preset }: { apiKey: string, preset?: any[] }) {
     this.openai = new OpenAI(apiKey);
-    this.types = types ? types : null;
+    this.presets = preset ? preset : null;
   }
 
-
-  static async write(type: WriterType | string, params: unknown) {
-    let writerType;
-    if (typeof type === 'string') {
-      writerType = this.types?.find(value => value.config.type === type);
+  private static getPreset(preset: Preset | string) {
+    if (typeof preset === 'string') {
+      const writerType = this.presets?.find(value => value.config.preset === preset);
 
       if (writerType === undefined) {
-        throw new Error(`Type ${type} not found.`);
+        throw new Error(`Type ${preset} not found.`);
       }
-    } else {
-      writerType = type;
+      return writerType;
     }
+    return preset;
+  }
 
+  private static validatePreset(preset: Preset, params: unknown) {
     try {
-      const writerParams = writerType.config.params;
+      const writerParams = preset.config.params;
       assert(params, writerParams);
     } catch (e: unknown) {
       if (e instanceof StructError) {
         const { message } = e;
-        throw new Error(`Typing verification for type ${writerType.config.type} failed. ${message}.`);
+        throw new Error(`Typing verification for type ${preset.config.preset} failed. ${message}.`);
       }
     }
+  }
 
-    const prompt = writerType.makeQuery(params as Infer<typeof writerType.config.params>);
+  static async write(rawPreset: Preset | string, params: unknown) {
+    const preset = this.getPreset(rawPreset);
+    this.validatePreset(preset, params);
+
+    const prompt = preset.makeQuery(params as Infer<typeof preset.config.params>);
+
+    if (Array.isArray(prompt)) {
+      return prompt.map(q => this.openai.createDavinciCompletion({ prompt: q }));
+    }
+
     return this.openai.createDavinciCompletion({ prompt });
   }
 }
-export type WriterType = { makeQuery: (params: unknown) => string, config: { type: string, params: Struct<any, object> } };
+export type Preset = { makeQuery: (params: unknown) => string | string[], config: { preset: string, params: Struct<any, object> } };
