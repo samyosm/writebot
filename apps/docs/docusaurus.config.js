6a53de9396c02cd2749cc1fb3fcd5cf514af62e3
Samy Rahmani <samy.osmium@gmail.com>
2023-01-14 15:25:41 -0500

    Changed url

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Writebot',
   tagline: 'OpenAI text generation abstraction.',
-  url: 'https://writebot-osmium-dev.vercel.app',
+  url: 'https://writebot.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
