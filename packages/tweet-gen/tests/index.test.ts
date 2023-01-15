5ab55cc4cd92ec7d51eb7d243b55d17a911a6a42
Samy Rahmani <samy.osmium@gmail.com>
2023-01-15 12:31:14 -0500

    test(tweet-gen): added tests

---
+++ packages/tweet-gen/tests/index.test.ts
@@ -0,0 +1,47 @@
+import TweetGen from '../index';
+
+describe('tweet-gen', () => {
+  describe('params', () => {
+    it('detects wrong param types', () => {
+      const params = {
+        description: 32, // supposed to be a string
+        tone: 'tone'
+      };
+
+      expect(TweetGen.config.params.is(params)).toBeFalsy();
+    });
+    it('detects wrong params', () => {
+      const params = {
+        title: 'Bla bla bla', // title doesn't exist.
+        tone: 'tone'
+      };
+
+      expect(TweetGen.config.params.is(params)).toBeFalsy();
+    });
+    it('detect good params', () => {
+      const params = {
+        description: 'hello',
+        tone: 'tone'
+      };
+
+      expect(TweetGen.config.params.is(params)).toBeTruthy();
+    });
+  });
+
+  describe('query', () => {
+    it('returns the right query', () => {
+      const params = {
+        tone: 'tone',
+        description: 'description'
+      };
+      expect(TweetGen.makeQuery(params)).toEqual(`
+  Generate a simple tweet with the following description and tone without any hashtags .
+  It is important for this tweet not to have any hashtags in it so do not put any.
+  Description: ${params.description}
+  Tone: ${params.tone}
+  `);
+    });
+
+    /* No need for undefined param test because all params are required here */
+  });
+});
