0f82932203d7bda64f73467146aca6af981e0697
Samy Rahmani <samy.osmium@gmail.com>
2023-01-18 07:26:12 -0500

    Initialized web app

---
+++ apps/web/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Writebot - Write with ease</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="A text editor that help you write." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
