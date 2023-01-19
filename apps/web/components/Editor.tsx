931fbe5f4f7afb985013309a28a9e725252763e6
Samy Rahmani <samy.osmium@gmail.com>
2023-01-18 20:22:29 -0500

    Added Basic Editor

--- apps/web/components/Editor.tsx
+++ apps/web/components/Editor.tsx
@@ -1,15 +1,83 @@
-import { useEditor, EditorContent } from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
+import { $getRoot, $getSelection, EditorState, EditorThemeClasses } from 'lexical';
+import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { useEffect } from 'react';
+import { HeadingNode, QuoteNode } from '@lexical/rich-text';
+import { TableCellNode, TableNode, TableRowNode } from '@lexical/table';
+import { ListItemNode, ListNode } from '@lexical/list';
+import { CodeHighlightNode, CodeNode } from '@lexical/code';
+import { AutoLinkNode, LinkNode } from '@lexical/link';
+import { MarkdownShortcutPlugin } from '@lexical/react/LexicalMarkdownShortcutPlugin';
+import { TRANSFORMERS } from '@lexical/markdown';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    // Focus the editor when the effect fires!
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}
+
+function onError(error: unknown) {
+  console.error(error);
+}
+
+function onChange(editorState: EditorState) {
+  editorState.read(() => {
+    // Read the contents of the EditorState here.
+/*    const root = $getRoot();
+    console.log(root.getTextContent().replace(/\n/g, ' '));
+    const selection = $getSelection();
+    console.log(selection?.getTextContent());*/
+
+  });
+}
+
+const theme: EditorThemeClasses = {
+  placeholder: 'text-white'
+};
 
 export const Editor = () => {
-  const editor = useEditor({
-    extensions: [
-      StarterKit
-    ],
-    content: '<p>Hello World! 🌎️</p>'
-  });
+  const initialConfig = {
+    namespace: 'MyEditor',
+    theme,
+    onError,
+    nodes: [
+      HeadingNode,
+      ListNode,
+      ListItemNode,
+      QuoteNode,
+      CodeNode,
+      CodeHighlightNode,
+      TableNode,
+      TableCellNode,
+      TableRowNode,
+      AutoLinkNode,
+      LinkNode
+    ]
+  };
 
   return (
-    <EditorContent editor={editor} />
+    <LexicalComposer initialConfig={initialConfig}>
+      <div className="w-full relative prose prose-sm prose-slate">
+        <RichTextPlugin
+          contentEditable={<ContentEditable className="relative w-full h-full bg-gray-200 p-5 prose prose-sm prose-slate"/>}
+          placeholder={<div className="absolute inset-5 select-none pointer-events-none prose prose-sm prose-slate">Enter some text...</div>}
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        <OnChangePlugin onChange={onChange} />
+        <HistoryPlugin />
+        <MyCustomAutoFocusPlugin/>
+        <MarkdownShortcutPlugin transformers={TRANSFORMERS} />
+      </div>
+    </LexicalComposer>
   );
 };
