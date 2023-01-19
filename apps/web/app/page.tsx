fdda1e6b360fd3b1d4a501b5908377c596ff7cfb
Samy Rahmani <samy.osmium@gmail.com>
2023-01-18 20:22:29 -0500

    Added Basic Editor

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
+
+import { Editor } from '@/components/Editor';
+
 export default function Home() {
   return (
-    <main className="min-h-screen w-full h-full flex justify-center bg-white">
+    <main className="min-h-screen w-full h-full flex justify-center bg-white p-5 text-black">
+      <Editor />
     </main>
   );
 }
